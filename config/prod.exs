81014abde616409ac617e64d06765c6ba1be6599
Most <mcadenhe@gmail.com>
2016-01-17 23:13:30 -0800

    Adjust prod config url options

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :pet_obituary, PetObituary.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "petnecrology.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
 	force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
